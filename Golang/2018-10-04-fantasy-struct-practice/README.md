6f9534a6650ae5c479d4a08893eb668a5550d038
ozyma <ozyma@ozymastudios.com>
2018-10-04 04:21:48 -0400

    fixed some issues and updated readme

--- Golang/2018-10-04-fantasy-struct-practice/README.md
+++ Golang/2018-10-04-fantasy-struct-practice/README.md
@@ -1,3 +1,23 @@
 # October 4th, 2018 - Fantasy struct praactice
 
-I am going to throw out struct ideas/methods for my PDM rogue-like for fun and practice 
+I am going to throw out struct ideas/methods for my PDM rogue-like for fun and practice
+
+I was unable to use this function earlier:
+```go
+func main() {
+	lysander := playerchar{"Lysander", 8, 9, 5, 12, 8, 19, 1490, 1129, 38.2, 31.0}
+	displayPlayerCharacterStats(lysander)
+}
+```
+
+And I realized it was because my function:
+```go
+func (pc *playerchar) displayPlayerCharacterStats() {
+    fmt.Println("Your character's name is:", pc.name)
+}
+```
+
+was a method for playerchar types, and therefore has to be called as such:
+```go
+lysander.displayPlayerCharacterStats()
+``` 
--- Golang/2018-10-04-fantasy-struct-practice/main.go
+++ Golang/2018-10-04-fantasy-struct-practice/main.go
@@ -3,17 +3,32 @@ package main
 import "fmt"
 
 type playerchar struct {
-	name string
-	STR  uint8
-	DEX  uint8
-	CON  uint8
-	INT  uint8
-	WIS  uint8
-	CHA  uint8
+	name                     string
+	STR                      uint8
+	DEX                      uint8
+	CON                      uint8
+	INT                      uint8
+	WIS                      uint8
+	CHA                      uint8
+	nextExpToLevel           uint64
+	currentExp               uint64
+	inventoryMaxKilograms    float64
+	inventorCurrentKilograms float64
+}
+
+func (pc *playerchar) displayPlayerCharacterStats() {
+	fmt.Println("Your character's name is:", pc.name)
+	fmt.Println("========================================")
+	fmt.Println("STR:", pc.STR)
+	fmt.Println("DEX:", pc.DEX)
+	fmt.Println("CON:", pc.CON)
+	fmt.Println("INT:", pc.INT)
+	fmt.Println("WIS:", pc.WIS)
+	fmt.Println("CHA:", pc.CHA)
+	fmt.Println("========================================")
 }
 
 func main() {
-	var lysander = playerchar{"lysander", 8, 9, 5, 12, 8, 19}
-	fmt.Println(lysander)
-
+	lysander := playerchar{"Lysander", 8, 9, 5, 12, 8, 19, 1490, 1129, 38.2, 31.0}
+	lysander.displayPlayerCharacterStats()
 }
