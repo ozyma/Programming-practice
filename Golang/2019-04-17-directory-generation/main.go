dded9434145dbf17836ce0b8c60d7a2eaf6ecb8c
ozyma <ozyma@ozymastudios.com>
2019-04-17 04:38:03 -0400

    Update main.go

--- Golang/2019-04-17-directory-generation/main.go
+++ Golang/2019-04-17-directory-generation/main.go
@@ -2,8 +2,13 @@ package main
 
 import(
 	"os"
+	"flag"
 )
 
 func main(){
-	os.Mkdir("test/", 0777)
+	mydir := flag.String("directory","test","input a directory you'd like to use")
+	flag.Parse()
+	
+	
+	os.Mkdir(*mydir, 0777)
 }
